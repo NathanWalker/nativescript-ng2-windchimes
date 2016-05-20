61b6eb4ab5132c1db534bb2ef2919eaf80f3955d
Josh Sommer <JoshDSommer@gmail.com>
2016-05-19 20:06:42 -0400

    ugh, merge conflicts

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,5 @@
 var bundler = require("nativescript-dev-webpack");
 
 module.exports = bundler.getConfig({
-<<<<<<< HEAD
-    // TODO: add project-specific webpack settings here...
-=======
->>>>>>> NathanWalker/master
+
 });
